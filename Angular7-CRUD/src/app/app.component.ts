d0f4169259a7cadede1da42a488cb7ec46d5db9e
Cristian Gonzalez <cdgl1996@gmail.com>
2019-08-16 11:20:30 -0400

    Added service to get user data

--- Angular7-CRUD/src/app/app.component.ts
+++ Angular7-CRUD/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PostServiceService } from './post-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Angular 7 - Consuming RESTful API';
+  public usersData = [];
+
+  createPostData;
+  createPostUser;
+  createPostTitle;
+  createPostBody;
+
+  constructor(private postService: PostServiceService) { }
+
+
+  ngOnInit() {
+
+    this.postService.getUsers().subscribe((data) => {
+      this.usersData = Array.from(Object.keys(data), k => data[k]);
+    });
+
+    this.createPostData = new FormGroup({
+      user: new FormControl(""),
+      title: new FormControl(""),
+      body: new FormControl("")
+    });
+
+  }
+
+  onSubmitCreatePost(data) {
+
+    this.createPostUser = data.user;
+    this.createPostTitle = data.title;
+    this.createPostBody = data.body;
+
+  }
+
 }
--- Angular7-CRUD/src/app/app.module.ts
+++ Angular7-CRUD/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule, MatOptionModule, MatSelectModule, MatIconModule } from '@angular/material';
 import { MatTabsModule } from '@angular/material';
 
@@ -16,6 +17,7 @@ import { MatTabsModule } from '@angular/material';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     MatInputModule,
